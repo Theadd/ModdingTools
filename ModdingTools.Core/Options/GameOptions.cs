bea29ddae2fe8d4622570dd72864bd8bfc0a3f4e
R. Beltran <rbeltran8000@gmail.com>
2022-12-10 18:08:06 +0100

    options as tuples

--- ModdingTools.Core/Options/GameOptions.cs
+++ ModdingTools.Core/Options/GameOptions.cs
@@ -5,6 +5,8 @@ public class GameOptions
     public DirectoryInfo? GamePath { get; set; }
     public string? GameName { get; set; }
     public string? UnityPlayerVersion { get; set; }
+    
+    public (DirectoryInfo, string, string) AsTuple() => (GamePath!, GameName!, UnityPlayerVersion!);
 
     public override string ToString() => $"\tGamePath = {GamePath!.FullName}\n\tGameName = {GameName}\n\tUnityPlayerVersion = {UnityPlayerVersion}";
 }

d2564d3a0060998111346544f7ac56a9a51c4d97
R. Beltran <rbeltran8000@gmail.com>
2022-12-10 15:19:44 +0100

    core project file + commandshell

--- ModdingTools.Core/CommandShell.cs
+++ ModdingTools.Core/CommandShell.cs
@@ -8,7 +8,28 @@ public class CommandShell
     public DirectoryInfo WorkingDirectory { get; set; } = new(".");
     public int CommandExecutionTimeout { get; set; } = 60000;   // In milliseconds
 
-    public bool Exec(string command)
+    public CommandShell GoBack() => Ignore(TryGo(".."));
+    
+    public CommandShell Go(string directoryName, bool createIfNotExists = false) => Ignore(TryGo(directoryName, createIfNotExists));
+
+    public bool TryGo(string directoryName, bool createIfNotExists = false)
+    {
+        var nextWorkingDir = new DirectoryInfo(Path.Combine(WorkingDirectory.FullName, directoryName));
+
+        if (!nextWorkingDir.Exists && !createIfNotExists)
+            return false;
+        
+        if (!nextWorkingDir.Exists)
+            nextWorkingDir.Create();
+
+        WorkingDirectory = nextWorkingDir;
+        
+        return true;
+    }
+    
+    public CommandShell Exec(string command) => Ignore(TryExec(command));
+    
+    public bool TryExec(string command)
     {
         var (cmdName, cmdArgs, _) = command.Split(' ', 2, StringSplitOptions.RemoveEmptyEntries);
         var process = new Process
@@ -31,4 +52,6 @@ public class CommandShell
         
         return true;    // TODO
     }
+
+    private CommandShell Ignore(bool _) => this;
 }

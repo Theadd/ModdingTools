df8a1974e1fe0394a73fa5228a6edbb2557283d9
R. Beltran <rbeltran8000@gmail.com>
2022-12-10 15:17:22 +0100

    options

---
+++ ModdingTools.Core/Options/GameOptions.cs
@@ -0,0 +1,12 @@
+﻿namespace ModdingTools.Core.Options;
+
+public class GameOptions
+{
+    public DirectoryInfo? GamePath { get; set; }
+    public string? GameName { get; set; }
+    public string? UnityPlayerVersion { get; set; }
+
+    public override string ToString() => $"\tGamePath = {GamePath!.FullName}\n\tGameName = {GameName}\n\tUnityPlayerVersion = {UnityPlayerVersion}";
+}
+
+
---
+++ ModdingTools.Core/Options/TemplateOptions.cs
@@ -0,0 +1,13 @@
+﻿namespace ModdingTools.Core.Options;
+
+public class TemplateOptions
+{
+    public DirectoryInfo? Location { get; set; }
+    public string? ProjectName { get; set; }
+    public string? AssemblyName { get; set; }
+    public string? SolutionName { get; set; }
+    public string? RootNamespace { get; set; }
+
+    public override string ToString() =>
+        $"\tLocation = {Location!.FullName}\n\tProjectName = {ProjectName}\n\tAssemblyName = {AssemblyName}\n\tSolutionName = {SolutionName}\n\tRootNamespace = {RootNamespace}";
+}

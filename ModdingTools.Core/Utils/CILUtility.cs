3c480341067367cb1814a7c4b242b7666913cde4
R. Beltran <rbeltran8000@gmail.com>
2022-12-10 07:11:03 +0100

    ported missing utils

---
+++ ModdingTools.Core/Utils/CILUtility.cs
@@ -0,0 +1,35 @@
+﻿using Mono.Cecil;
+
+namespace ModdingTools.Core.Utils;
+
+public class CILUtility
+{
+    public void MakePublic(AssemblyDefinition original)
+    {
+        foreach (TypeDefinition typeDefinition in original.MainModule.Types.Recursive())
+        {
+            if (typeDefinition.IsNested)
+                typeDefinition.IsNestedPublic = true;
+            else
+                typeDefinition.IsPublic = true;
+        }
+    }
+}
+
+public static class CILExtensions
+{
+    public static IEnumerable<TypeDefinition> Recursive(
+        this IEnumerable<TypeDefinition> self)
+    {
+        foreach (TypeDefinition t in self)
+        {
+            yield return t;
+            if (t.HasNestedTypes)
+            {
+                foreach (TypeDefinition typeDefinition in t.NestedTypes.AsEnumerable<TypeDefinition>()
+                             .Recursive())
+                    yield return typeDefinition;
+            }
+        }
+    }
+}
---
+++ ModdingTools.Core/Utils/GameUtility.cs
@@ -0,0 +1,62 @@
+﻿using Mono.Cecil;
+using System;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace ModdingTools.Core.Utils;
+
+public class GameUtility
+{
+    private const string MainDLLFileName = "Assembly-CSharp.dll";
+
+    public static Version GetCurrentGameVersion(string baseDllPath)
+    {
+        DefaultAssemblyResolver assemblyResolver = new DefaultAssemblyResolver();
+        MetadataResolver metadataResolver = new MetadataResolver((IAssemblyResolver) assemblyResolver);
+        assemblyResolver.AddSearchDirectory(baseDllPath);
+        ReaderParameters parameters = new ReaderParameters()
+        {
+            MetadataResolver = (IMetadataResolver) metadataResolver
+        };
+        using (AssemblyDefinition assemblyDefinition =
+               AssemblyDefinition.ReadAssembly(Path.Combine(baseDllPath, "Assembly-CSharp.dll"),
+                   parameters))
+        {
+            CustomAttribute customAttribute =
+                assemblyDefinition.CustomAttributes.FirstOrDefault<CustomAttribute>(
+                    (Func<CustomAttribute, bool>) (x => x.AttributeType.Name == "VersionAttribute"));
+            CustomAttributeArgument attributeArgument = customAttribute.Fields
+                .FirstOrDefault<CustomAttributeNamedArgument>(
+                    (Func<CustomAttributeNamedArgument, bool>) (x => x.Name == "Major")).Argument;
+            short major = (short) attributeArgument.Value;
+            attributeArgument = customAttribute.Fields
+                .FirstOrDefault<CustomAttributeNamedArgument>(
+                    (Func<CustomAttributeNamedArgument, bool>) (x => x.Name == "Minor")).Argument;
+            short minor = (short) attributeArgument.Value;
+            attributeArgument = customAttribute.Fields
+                .FirstOrDefault<CustomAttributeNamedArgument>(
+                    (Func<CustomAttributeNamedArgument, bool>) (x => x.Name == "Revision")).Argument;
+            short revision = (short) attributeArgument.Value;
+            attributeArgument = customAttribute.Fields
+                .FirstOrDefault<CustomAttributeNamedArgument>(
+                    (Func<CustomAttributeNamedArgument, bool>) (x => x.Name == "Build")).Argument;
+            int build = (int) attributeArgument.Value;
+            attributeArgument = customAttribute.Fields
+                .FirstOrDefault<CustomAttributeNamedArgument>(
+                    (Func<CustomAttributeNamedArgument, bool>) (x => x.Name == "Label")).Argument;
+            string str = (string) attributeArgument.Value;
+            return new Version((int) major, (int) minor, build, (int) revision);
+        }
+    }
+
+    public static string GetDefaultPath()
+    {
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            return Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),
+                "/Library/Application Support/Steam/steamapps/common/Humankind");
+        return RuntimeInformation.IsOSPlatform(OSPlatform.Windows)
+            ? "C:\\Program Files (x86)\\Steam\\steamapps\\common\\Humankind"
+            : string.Empty;
+    }
+}

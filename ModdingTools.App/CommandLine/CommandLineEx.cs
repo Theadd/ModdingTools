7eeec40b76eb9c95358208e66ac1787c32c52e1c
R. Beltran <rbeltran8000@gmail.com>
2022-12-21 23:34:18 +0100

    commandline helper class + extensions methods

---
+++ ModdingTools.App/CommandLine/CommandLineEx.cs
@@ -0,0 +1,33 @@
+﻿using System.CommandLine;
+
+namespace ModdingTools.App.CommandLine;
+
+/// <summary>
+/// System.CommandLine related extension methods.
+/// </summary>
+public static class CommandLineEx
+{
+    public static Argument<DirectoryInfo?> Hide(this Argument<DirectoryInfo?> self)
+    {
+        self.IsHidden = true;
+        return self;
+    }
+    
+    public static Argument<string?> Hide(this Argument<string?> self)
+    {
+        self.IsHidden = true;
+        return self;
+    }
+    
+    public static Option<DirectoryInfo?> Hide(this Option<DirectoryInfo?> self)
+    {
+        self.IsHidden = true;
+        return self;
+    }
+    
+    public static Option<string?> Hide(this Option<string?> self)
+    {
+        self.IsHidden = true;
+        return self;
+    }
+}
---
+++ ModdingTools.App/CommandLine/CommandLineHelper.cs
@@ -0,0 +1,26 @@
+﻿using ModdingTools.Core;
+
+namespace ModdingTools.App.CommandLine;
+
+public class CommandLineHelper
+{
+    private static IEnumerable<ConsoleColor> ColorPaletteA = new[]
+        { ConsoleColor.DarkGray, ConsoleColor.Green, ConsoleColor.White };
+    private static IEnumerable<ConsoleColor> ColorPaletteB = new[]
+        { ConsoleColor.DarkGray, ConsoleColor.Green, ConsoleColor.Yellow };
+
+    public static void DisplayCommandShellEntry(CommandShell.CommandShellEntry entry)
+    {
+        if (entry.QuietMode) return;
+
+        var palette = (entry.CommandName is "Write" or "dotnet" or "Unzip" ? ColorPaletteB : ColorPaletteA).ToArray();
+
+        Console.ForegroundColor = palette.ElementAt(0);
+        Console.Write("$ ");
+        Console.ForegroundColor = palette.ElementAt(1);
+        Console.Write($"{entry.CommandName}");
+        Console.ForegroundColor = palette.ElementAt(2);
+        Console.WriteLine($" {entry.CommandArgs}");
+        Console.ResetColor();
+    }
+}

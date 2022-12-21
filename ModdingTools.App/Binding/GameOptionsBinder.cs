23649995d78905e6e4e2bdefb3d921ba899de694
R. Beltran <rbeltran8000@gmail.com>
2022-12-22 00:50:53 +0100

    improved options binding

--- ModdingTools.App/Binding/GameOptionsBinder.cs
+++ ModdingTools.App/Binding/GameOptionsBinder.cs
@@ -6,17 +6,43 @@ namespace ModdingTools.App.Binding;
 
 public class GameOptionsBinder : BinderBase<GameOptions>
 {
-    private readonly Option<DirectoryInfo> _gamePath;
+    private readonly Option<DirectoryInfo?> _gamePath;
+    private readonly Argument<DirectoryInfo?> _targetPath;
 
-    public GameOptionsBinder(Option<DirectoryInfo?> gamePath)
+    public GameOptionsBinder(Option<DirectoryInfo?> gamePath, Argument<DirectoryInfo?> targetPath)
     {
-        _gamePath = gamePath!;
+        _gamePath = gamePath;
+        _targetPath = targetPath;
     }
 
     protected override GameOptions GetBoundValue(BindingContext bindingContext)
     {
-        var dir = bindingContext.ParseResult.GetValueForOption(_gamePath)!;
-        var gameName = BindingOptionsHelper.GetGameName(dir);
+        var isValid = false;
+        var gameName = "";
+
+        var dir = bindingContext.ParseResult.GetValueForOption(_gamePath);
+
+        if (dir != null)
+        {
+            gameName = BindingOptionsHelper.GetGameName(dir);
+            if (!string.IsNullOrWhiteSpace(gameName))
+                isValid = true;
+        }
+        
+        if (!isValid)
+        {
+            var workingPath = bindingContext.ParseResult.GetValueForArgument(_targetPath);
+            
+            if (workingPath != null)
+            {
+                var auxGamePath = BindingOptionsHelper.GetGamePathFromConfigurationFilesFrom(workingPath);
+                if (auxGamePath != null)
+                {
+                    dir = auxGamePath;
+                    gameName = BindingOptionsHelper.GetGameName(dir);
+                }
+            }
+        }
 
         return new GameOptions
         {
--- ModdingTools.App/Binding/TemplateOptionsBinder.cs
+++ ModdingTools.App/Binding/TemplateOptionsBinder.cs
@@ -6,20 +6,20 @@ namespace ModdingTools.App.Binding;
 
 public class TemplateOptionsBinder : BinderBase<TemplateOptions>
 {
-    private readonly Argument<DirectoryInfo> _location;
-    private readonly Option<string> _projectName;
-    private readonly Option<string> _assemblyName;
-    private readonly Option<string> _solutionName;
-    private readonly Option<string> _rootNamespace;
-    private readonly Option<string> _templateShortName;
+    private readonly Argument<DirectoryInfo?> _location;
+    private readonly IValueDescriptor<string?> _projectName;
+    private readonly Option<string?> _assemblyName;
+    private readonly Option<string?> _solutionName;
+    private readonly Option<string?> _rootNamespace;
+    private readonly Option<string?> _templateShortName;
 
     public TemplateOptionsBinder(
-        Argument<DirectoryInfo> location,
-        Option<string> projectName,
-        Option<string> assemblyName,
-        Option<string> solutionName,
-        Option<string> rootNamespace,
-        Option<string> templateShortName)
+        Argument<DirectoryInfo?> location,
+        IValueDescriptor<string?> projectName,
+        Option<string?> assemblyName,
+        Option<string?> solutionName,
+        Option<string?> rootNamespace,
+        Option<string?> templateShortName)
     {
         _location = location;
         _projectName = projectName;
@@ -31,12 +31,17 @@ public class TemplateOptionsBinder : BinderBase<TemplateOptions>
 
     protected override TemplateOptions GetBoundValue(BindingContext bindingContext)
     {
-        var location = bindingContext.ParseResult.GetValueForArgument(_location)!;
+        var location = bindingContext.ParseResult.GetValueForArgument(_location) ?? new DirectoryInfo(".");
         var sanitizedLocationName = location.Name.Replace(' ', '.');
-        var projectName = bindingContext.ParseResult.GetValueForOption(_projectName) ?? sanitizedLocationName;
+        var projectName = "";
+        
+        if (_projectName is Option<string?> pNameOption)
+            projectName = bindingContext.ParseResult.GetValueForOption(pNameOption) ?? sanitizedLocationName;
+        else if (_projectName is Argument<string?> pNameArgument)
+            projectName = bindingContext.ParseResult.GetValueForArgument(pNameArgument) ?? sanitizedLocationName;
+        
         var assemblyName = bindingContext.ParseResult.GetValueForOption(_assemblyName) ?? projectName;
-        var solutionName = bindingContext.ParseResult.GetValueForOption(_solutionName) ??
-                           sanitizedLocationName;
+        var solutionName = bindingContext.ParseResult.GetValueForOption(_solutionName) ?? sanitizedLocationName;
         var rootNamespace = bindingContext.ParseResult.GetValueForOption(_rootNamespace) ?? projectName;
         var templateShortName = bindingContext.ParseResult.GetValueForOption(_templateShortName) ?? "";
 

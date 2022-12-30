a4d9bd8b5b08ada8f37e1778898e59eeaffaaa09
R. Beltran <rbeltran8000@gmail.com>
2022-12-30 05:07:55 +0100

    update Program.cs to include CommandAdd and CommandGameInfo

--- ModdingTools.App/Program.cs
+++ ModdingTools.App/Program.cs
@@ -6,24 +6,26 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using ModdingTools.App.CommandLine;
 using ModdingTools.Core.Options;
-// using ModdingTools.Templates.Writers;
 
 namespace ModdingTools.App;
 
 class Program
 {
-    // S:\Program Files\Steam\steamapps\common\Humankind
     public static async Task<int> Main(string[] args)
     {
         var rootCommand = new RootCommand("ModdingTools CLI");
 
         rootCommand.AddGlobalOption(CommandLineArgs.DryRunOption);
+        rootCommand.AddGlobalOption(CommandLineArgs.QuietOption);
         var createCommand = CommandNew.Create();
         var infoCommand = CommandInfo.Create();
+        var addCommand = CommandAdd.Create();
+        var gameInfoCommand = CommandGameInfo.Create();
         rootCommand.AddCommand(createCommand);
         rootCommand.AddCommand(infoCommand);
-
-
+        rootCommand.AddCommand(addCommand);
+        rootCommand.AddCommand(gameInfoCommand);
+        
         try
         {
             return await new CommandLineBuilder(rootCommand)
@@ -35,34 +37,26 @@ class Program
                         .ConfigureServices(services =>
                         {
                             services.AddOptions<GameOptions>().BindConfiguration("GameOptions");
-
-                            // services.AddTransient<InitialStructure>();
-                            /*services.AddHttpClient("unity", client =>
-                            {
-                                client.BaseAddress = new Uri("https://unity3d.com/");
-                            }).AddTransientHttpErrorPolicy(policy => policy.WaitAndRetryAsync(5,
-                                retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt))));*/
                         });
-                    //.UseCommandHandler<MineCommand, MineCommand.Handler>();
                     /*.UseSerilog((context, services, loggerConfiguration) => loggerConfiguration
                         .ReadFrom.Configuration(context.Configuration)
                         .Enrich.FromLogContext()
                         .WriteTo.Console());*/
                 })
                 .UseDefaults()
-                .UseExceptionHandler(
-                    (ex, _) => Console.WriteLine("Exception, cannot continue! " + ex.Message), -1)
+                // .UseExceptionHandler(
+                 //   (ex, _) => Console.WriteLine("Exception, cannot continue! " + ex.Message), -1)
                 .Build()
                 .InvokeAsync(args);
         }
         catch (Exception e)
         {
             Console.WriteLine("EXCEPTION: " + e.Message);
-            // await Task.Delay(100);
+            Console.WriteLine(e);
+
             return 0;
         }
-
-
+        
         // return 0;
     }
 }

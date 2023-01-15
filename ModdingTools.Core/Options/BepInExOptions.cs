903f84f2c1a2cf855775d067dcc8635290b5f2db
R. Beltran <rbeltran8000@gmail.com>
2023-01-16 00:47:51 +0100

    add BepInExOptions

---
+++ ModdingTools.Core/Options/BepInExOptions.cs
@@ -0,0 +1,19 @@
+﻿namespace ModdingTools.Core.Options;
+
+public record BepInExOptionsRecord(
+    DirectoryInfo BepInExPath,
+    string BepInExVersion);
+
+public class BepInExOptions
+{
+    public DirectoryInfo? BepInExPath { get; set; }
+    public string? BepInExVersion { get; set; }
+    
+    public (DirectoryInfo, string) AsTuple() => 
+        (BepInExPath!, BepInExVersion!);
+
+    public BepInExOptionsRecord AsRecord() =>
+        new (BepInExPath!, BepInExVersion!);
+
+    public override string ToString() => $"\tBepInExPath = {BepInExPath?.FullName}\n\tBepInExVersion = {BepInExVersion}";
+}
